9e21b25ab130ee5fb4174d6b526961cdfa273603
qiubix <qiubix@gmail.com>
2017-11-24 21:35:17 +0100

    Fix failing web tests

--- rekolekcje-webapp/src/app/app.component.spec.ts
+++ rekolekcje-webapp/src/app/app.component.spec.ts
@@ -1,13 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import {ParticipantsComponent} from "./participants/participants.component";
+import {MockParticipantsService} from "./participants/mock-participants.service";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ParticipantsComponent
       ],
+      providers: [
+        MockParticipantsService
+      ]
     }).compileComponents();
   }));
 
@@ -20,13 +26,13 @@ describe('AppComponent', () => {
   it(`should have as title 'app works!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Rekolekcje');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Rekolekcje');
   }));
 });
