dc7cbe1acfcd1355d6d17f3b82e669002567fb57
qiubix <qiubix@gmail.com>
2017-11-27 18:21:06 +0100

    Implement modal as a shared component

---
+++ rekolekcje-webapp/src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalComponent } from './modal/modal.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    ModalComponent
+  ]
+})
+export class SharedModule { }
