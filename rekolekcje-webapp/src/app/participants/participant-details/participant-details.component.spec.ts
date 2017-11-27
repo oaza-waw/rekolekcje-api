a12b29b5d3f4b471b6aaa4f46c6610ba3e7ca49f
qiubix <qiubix@gmail.com>
2017-11-27 17:42:52 +0100

    Fix web tests failing after routing implementation

--- rekolekcje-webapp/src/app/participants/participant-details/participant-details.component.spec.ts
+++ rekolekcje-webapp/src/app/participants/participant-details/participant-details.component.spec.ts
@@ -1,16 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ParticipantDetailsComponent } from './participant-details.component';
+import {MockParticipantsService} from "../mock-participants.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Location} from "@angular/common";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('ParticipantDetailsComponent', () => {
   let component: ParticipantDetailsComponent;
   let fixture: ComponentFixture<ParticipantDetailsComponent>;
+  let router: Router;
+  let location: Location;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ParticipantDetailsComponent ]
+      imports: [ RouterTestingModule ],
+      declarations: [ ParticipantDetailsComponent ],
+      providers: [ MockParticipantsService ]
     })
     .compileComponents();
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
   }));
 
   beforeEach(() => {
