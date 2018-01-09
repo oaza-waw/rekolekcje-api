71bdbe61ada28840412a0eaa5f3c1771290d2975
qiubix <qiubix@gmail.com>
2018-01-09 16:27:13 +0100

    Cleanup dependencies, remove duplicating test changesets

--- rekolekcje-engine/src/test/resources/pl.oaza.warszawa.dor.rekolekcje/changelogs.groovy
+++
@@ -1,6 +0,0 @@
-package pl.oaza.warszawa.dor.rekolekcje
-
-databaseChangeLog {
-  include(file: 'changelog/initial.groovy', relativeToChangelogFile: true)
-  include(file: 'changelog/security.groovy', relativeToChangelogFile: true)
-}
