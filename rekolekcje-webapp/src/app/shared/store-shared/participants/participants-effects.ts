0c7e95f3df3cc607ba61d4b367e511338abb9404
wjanaszek <trash.wjanaszek@gmail.com>
2018-02-12 21:11:04 +0100

    New version RxJS operators

--- rekolekcje-webapp/src/app/shared/store-shared/participants/participants-effects.ts
+++ rekolekcje-webapp/src/app/shared/store-shared/participants/participants-effects.ts
@@ -1,27 +1,23 @@
 import { Action, Store } from '@ngrx/store';
 import { Actions, Effect } from '@ngrx/effects';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Participants } from '../../../core/store/participants/participants-reducer';
 import { ParticipantsSharedActions } from './participants-actions';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import CreateParticipantSuccess = ParticipantsSharedActions.CreateParticipantSuccess;
 import { of } from 'rxjs/observable/of';
-import DeleteParticipantSuccess = ParticipantsSharedActions.DeleteParticipantSuccess;
-import { MOCK_PARTICIPANTS } from '../../../mock-data/mock-participants';
-import UpdateParticipantSuccess = ParticipantsSharedActions.UpdateParticipantSuccess;
 import { Injectable } from '@angular/core';
-import SelectParticipantSuccess = ParticipantsSharedActions.SelectParticipantSuccess;
 import { Config } from '../../../../config';
 import { Participant } from 'app/shared/models/participant.model';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import CreateParticipantSuccess = ParticipantsSharedActions.CreateParticipantSuccess;
+import DeleteParticipantSuccess = ParticipantsSharedActions.DeleteParticipantSuccess;
+import UpdateParticipantSuccess = ParticipantsSharedActions.UpdateParticipantSuccess;
+import SelectParticipantSuccess = ParticipantsSharedActions.SelectParticipantSuccess;
 import CreateParticipantFail = ParticipantsSharedActions.CreateParticipantFail;
 import DeleteParticipantFail = ParticipantsSharedActions.DeleteParticipantFail;
 import LoadParticipantsListSuccess = ParticipantsSharedActions.LoadParticipantsListSuccess;
 import LoadParticipantsListFail = ParticipantsSharedActions.LoadParticipantsListFail;
 import UpdateParticipantFail = ParticipantsSharedActions.UpdateParticipantFail;
-import { AuthService } from '../../../auth/auth.service';
 
 @Injectable()
 export class ParticipantsEffects {
@@ -29,44 +25,58 @@ export class ParticipantsEffects {
   @Effect()
   CreateParticipant: Observable<Action> = this.actions
     .ofType(ParticipantsSharedActions.types.CreateParticipant)
-    .switchMap((action: ParticipantsSharedActions.CreateParticipant) =>
-      this.http.post<Participant>(Config.endpoints.participantsModule, action.payload)
-        .map(data => new CreateParticipantSuccess(data))
-        .catch(error => of(new CreateParticipantFail(error))));
+    .pipe(
+      switchMap((action: ParticipantsSharedActions.CreateParticipant) =>
+        this.http.post<Participant>(Config.endpoints.participantsModule, action.payload)
+          .pipe(
+            map(data => new CreateParticipantSuccess(data)),
+            catchError(error => of(new CreateParticipantFail(error)))
+          )
+      )
+    );
 
   @Effect()
   DeleteParticipant: Observable<Action> = this.actions
     .ofType(ParticipantsSharedActions.types.DeleteParticipant)
-    .switchMap((action: ParticipantsSharedActions.DeleteParticipant) =>
-      this.http.delete<Participant>(Config.endpoints.participantsModule + '/' + action.payload)
-        .map(data => new DeleteParticipantSuccess(action.payload))
-        .catch(error => of(new DeleteParticipantFail(error))));
+    .pipe(
+      switchMap((action: ParticipantsSharedActions.DeleteParticipant) =>
+        this.http.delete<Participant>(Config.endpoints.participantsModule + '/' + action.payload)
+          .pipe(
+            map(data => new DeleteParticipantSuccess(action.payload)),
+            catchError(error => of(new DeleteParticipantFail(error)))
+          ))
+    );
 
-  // Uncomment if you want data from mock data component
-  // @Effect()
-  // LoadParticipantsList: Observable<Action> = this.actions
-  //   .ofType(ParticipantsSharedActions.types.LoadParticipantsList)
-  //   .switchMap((action: ParticipantsSharedActions.LoadParticipantsList) => of(new LoadParticipantsListSuccess(MOCK_PARTICIPANTS)));
   @Effect()
   LoadParticipantsList: Observable<Action> = this.actions
     .ofType(ParticipantsSharedActions.types.LoadParticipantsList)
-    .switchMap((action: ParticipantsSharedActions.LoadParticipantsList) =>
-      this.http.get<Participant[]>(Config.endpoints.participantsModule)
-        .map((data: Participant[]) => new LoadParticipantsListSuccess(data))
-        .catch(error => of(new LoadParticipantsListFail(error))));
+    .pipe(
+      switchMap((action: ParticipantsSharedActions.LoadParticipantsList) =>
+        this.http.get<Participant[]>(Config.endpoints.participantsModule)
+          .pipe(
+            map((data: Participant[]) => new LoadParticipantsListSuccess(data)),
+            catchError(error => of(new LoadParticipantsListFail(error)))
+          ))
+    );
 
   @Effect()
   SelectParticipant: Observable<Action> = this.actions
     .ofType(ParticipantsSharedActions.types.SelectParticipant)
-    .switchMap((action: ParticipantsSharedActions.SelectParticipant) => of(new SelectParticipantSuccess(action.payload)));
+    .pipe(
+      switchMap((action: ParticipantsSharedActions.SelectParticipant) => of(new SelectParticipantSuccess(action.payload)))
+    );
 
   @Effect()
   UpdateParticipant: Observable<Action> = this.actions
     .ofType(ParticipantsSharedActions.types.UpdateParticipant)
-    .switchMap((action: ParticipantsSharedActions.UpdateParticipant) =>
-      this.http.put<Participant>(Config.endpoints.participantsModule, action.payload)
-        .map(data => new UpdateParticipantSuccess(data))
-        .catch(error => of(new UpdateParticipantFail(error))));
+    .pipe(
+      switchMap((action: ParticipantsSharedActions.UpdateParticipant) =>
+        this.http.put<Participant>(Config.endpoints.participantsModule, action.payload)
+          .pipe(
+            map(data => new UpdateParticipantSuccess(data)),
+            catchError(error => of(new UpdateParticipantFail(error)))
+          ))
+    );
 
   constructor(private actions: Actions, private store: Store<Participants.State>, private http: HttpClient) {
   }
