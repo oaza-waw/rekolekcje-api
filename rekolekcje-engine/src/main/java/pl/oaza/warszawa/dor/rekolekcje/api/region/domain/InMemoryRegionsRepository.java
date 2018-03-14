269463533524c668964e47d0b17792904a9bad7a
Wojciech Matusiak <wm382710@students.mimuw.edu.pl>
2018-03-14 17:54:05 +0100

    Implemented RegionsService with tests

--- rekolekcje-engine/src/main/java/pl/oaza/warszawa/dor/rekolekcje/api/region/domain/InMemoryRegionsRepository.java
+++ rekolekcje-engine/src/main/java/pl/oaza/warszawa/dor/rekolekcje/api/region/domain/InMemoryRegionsRepository.java
@@ -2,11 +2,39 @@ package pl.oaza.warszawa.dor.rekolekcje.api.region.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static java.util.Objects.requireNonNull;
 
 class InMemoryRegionsRepository implements RegionsRepository {
 
+  AtomicLong nextId = new AtomicLong(1);
+  ConcurrentMap<Long, Region> data = new ConcurrentHashMap<>();
+
   @Override
   public List<Region> findAll() {
-    return new ArrayList<>();
+    return new ArrayList<>(data.values());
+  }
+
+  @Override
+  public Region findOne(long id) {
+    return data.get(id);
+  }
+
+  @Override
+  public Region save(Region region) {
+    requireNonNull(region);
+    if (region.getId() == null || !data.containsKey(region.getId())) {
+      region = region.withId(nextId.getAndIncrement());
+    }
+    data.put(region.getId(), region);
+    return region;
+  }
+
+  @Override
+  public void delete(long id) {
+    data.remove(id);
   }
 }
