b32ecb4ee8889ae73d6f6f3ba531d1779a59cd79
qiubix <qiubix@gmail.com>
2018-04-18 17:08:30 +0200

    Unify Jackson configuration between app and tests

--- rekolekcje-engine/src/main/java/pl/oaza/warszawa/dor/rekolekcje/api/tools/JacksonConfig.java
+++ rekolekcje-engine/src/main/java/pl/oaza/warszawa/dor/rekolekcje/api/tools/JacksonConfig.java
@@ -1,22 +1,21 @@
 package pl.oaza.warszawa.dor.rekolekcje.api.tools;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 class JacksonConfig {
 
-  @Bean
-  @Primary
-  public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
-    ObjectMapper objectMapper = builder.createXmlMapper(false).build();
-    objectMapper.registerModule(new JavaTimeModule());
-    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-    return objectMapper;
-  }
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
+        return builder -> {
+            builder.createXmlMapper(false).build();
+            final JavaTimeModule javaTimeModule = new JavaTimeModule();
+            builder.modules(javaTimeModule);
+            builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        };
+    }
 }
