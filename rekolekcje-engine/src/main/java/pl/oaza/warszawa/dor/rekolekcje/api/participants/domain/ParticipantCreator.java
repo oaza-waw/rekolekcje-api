05573d4fb4cbcb6073cbbc1ed5b2102a5ab40a0f
KTryCode <32237739+KTryCode@users.noreply.github.com>
2018-04-20 21:45:24 +0200

    Backend: domain: ParticipantCreator changed, added heath status fields

--- rekolekcje-engine/src/main/java/pl/oaza/warszawa/dor/rekolekcje/api/participants/domain/ParticipantCreator.java
+++ rekolekcje-engine/src/main/java/pl/oaza/warszawa/dor/rekolekcje/api/participants/domain/ParticipantCreator.java
@@ -7,37 +7,45 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
 class ParticipantCreator {
-  Participant from(ParticipantDTO participantDTO) {
-    return Participant.builder()
-        .id(participantDTO.getId())
-        .firstName(participantDTO.getFirstName())
-        .lastName(participantDTO.getLastName())
-        .pesel(participantDTO.getPesel())
-        .address(participantDTO.getAddress())
-        .parishId(participantDTO.getParishId())
-        .motherName(participantDTO.getMotherName())
-        .fatherName(participantDTO.getFatherName())
-        .christeningPlace(participantDTO.getChristeningPlace())
-        .christeningDate(convertToDateTime(participantDTO.getChristeningDate()))
-        .build();
-  }
+    Participant from(ParticipantDTO participantDTO) {
+        return Participant.builder()
+                .id(participantDTO.getId())
+                .firstName(participantDTO.getFirstName())
+                .lastName(participantDTO.getLastName())
+                .pesel(participantDTO.getPesel())
+                .address(participantDTO.getAddress())
+                .parishId(participantDTO.getParishId())
+                .motherName(participantDTO.getMotherName())
+                .fatherName(participantDTO.getFatherName())
+                .christeningPlace(participantDTO.getChristeningPlace())
+                .christeningDate(convertToDateTime(participantDTO.getChristeningDate()))
+                .currentTreatment(participantDTO.getCurrentTreatment())
+                .medications(participantDTO.getMedications())
+                .allergies(participantDTO.getAllergies())
+                .other(participantDTO.getOther())
+                .build();
+    }
 
-  private LocalDateTime convertToDateTime(ZonedDateTime zonedDateTime) {
-    return zonedDateTime != null ? zonedDateTime.withZoneSameInstant(ZoneOffset.UTC).toLocalDateTime() : null;
-  }
+    private LocalDateTime convertToDateTime(ZonedDateTime zonedDateTime) {
+        return zonedDateTime != null ? zonedDateTime.withZoneSameInstant(ZoneOffset.UTC).toLocalDateTime() : null;
+    }
 
-  Participant withId(Participant participant, Long id) {
-    return Participant.builder()
-        .id(id)
-        .firstName(participant.getFirstName())
-        .lastName(participant.getLastName())
-        .pesel(participant.getPesel())
-        .address(participant.getAddress())
-        .parishId(participant.getParishId())
-        .motherName(participant.getMotherName())
-        .fatherName(participant.getFatherName())
-        .christeningDate(participant.getChristeningDate())
-        .christeningPlace(participant.getChristeningPlace())
-        .build();
-  }
+    Participant withId(Participant participant, Long id) {
+        return Participant.builder()
+                .id(id)
+                .firstName(participant.getFirstName())
+                .lastName(participant.getLastName())
+                .pesel(participant.getPesel())
+                .address(participant.getAddress())
+                .parishId(participant.getParishId())
+                .motherName(participant.getMotherName())
+                .fatherName(participant.getFatherName())
+                .christeningDate(participant.getChristeningDate())
+                .christeningPlace(participant.getChristeningPlace())
+                .currentTreatment(participant.getCurrentTreatment())
+                .medications(participant.getMedications())
+                .allergies(participant.getAllergies())
+                .other(participant.getOther())
+                .build();
+    }
 }
