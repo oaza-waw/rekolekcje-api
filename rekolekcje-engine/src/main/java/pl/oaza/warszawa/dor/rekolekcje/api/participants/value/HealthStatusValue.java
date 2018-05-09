1dd14b7b38a4b88ea795364165e167860811725d
KTryCode <32237739+KTryCode@users.noreply.github.com>
2018-05-09 21:12:55 +0200

    HealthStatus and HealthStatusValue added

---
+++ rekolekcje-engine/src/main/java/pl/oaza/warszawa/dor/rekolekcje/api/participants/value/HealthStatusValue.java
@@ -0,0 +1,13 @@
+package pl.oaza.warszawa.dor.rekolekcje.api.participants.value;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class HealthStatusValue {
+  private final String currentTreatment;
+  private final String medications;
+  private final String allergies;
+  private final String other;
+}
