1221e139bf051dd0ff8e5e620c964c5f6a330236
Wojciech Matusiak <wm382710@students.mimuw.edu.pl>
2018-05-14 18:20:57 +0200

    Fixed not passing tests verify; first passing test for /api/regions
    * Tests were not passing due to database changeset written in SQL.
      H2 seems to have different syntax than PostgreSQL sometimes, so
      changelog was rewritten in xml
    * Added field region to ParishData (maybe i should now modify
      the tests?)
    * Removed this stupid line from WebSecurityConfig...
    * First /api/region endpoint test (selecting all regions)

--- rekolekcje-engine/src/verify/java/pl/oaza/warszawa/dor/rekolekcje/api/core/Database.java
+++ rekolekcje-engine/src/verify/java/pl/oaza/warszawa/dor/rekolekcje/api/core/Database.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
 import pl.oaza.warszawa.dor.rekolekcje.api.parish.ParishData;
 import pl.oaza.warszawa.dor.rekolekcje.api.parish.dto.ParishNotFoundException;
+import pl.oaza.warszawa.dor.rekolekcje.api.region.RegionData;
 
 public class Database {
 
@@ -53,6 +54,15 @@ public class Database {
 
   public void clearParish() {
     jdbcTemplate.execute("DELETE FROM parish");
+  }
+
+  public List<RegionData> getAllRegions() {
+    return jdbcTemplate.query("SELECT * FROM region",
+      (rs, rowNum) -> new RegionData(rs.getLong("id"), rs.getString("name")));
+  }
+
+  public void clearRegion() {
+    jdbcTemplate.execute("DELETE FROM region");
   }
 
 }
---
+++ rekolekcje-engine/src/verify/java/pl/oaza/warszawa/dor/rekolekcje/api/region/RegionAcceptanceTest.java
@@ -0,0 +1,51 @@
+package pl.oaza.warszawa.dor.rekolekcje.api.region;
+
+import org.junit.After;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import pl.oaza.warszawa.dor.rekolekcje.api.core.BaseIntegrationTest;
+import pl.oaza.warszawa.dor.rekolekcje.api.region.domain.RegionsRepository;
+import pl.oaza.warszawa.dor.rekolekcje.api.region.domain.RegionsService;
+import pl.oaza.warszawa.dor.rekolekcje.api.region.dto.RegionDTO;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class RegionAcceptanceTest extends BaseIntegrationTest {
+
+  private final RegionDTO region1 = new RegionDTO(1L, "Kryta");
+  private final RegionDTO region2= new RegionDTO(2L, "Maguuma Jungle");
+  private final List<RegionDTO> allRegions = Arrays.asList(region1, region2);
+
+  @Autowired
+  private RegionsService service;
+
+  @After
+  public void tearDown() {
+    database.clearRegion();
+  }
+
+  @WithMockUser
+  @Test
+  public void shouldGetAllRegions() throws Exception {
+    // given system has some parishes
+    allRegions.forEach(r -> service.save(r));
+    final MockHttpServletRequestBuilder getAllRequest = get("/api/regions");
+
+    // when requesting all parishes
+    final ResultActions response = mockMvc.perform(getAllRequest);
+
+    // all are returned
+    final List<RegionData> result = database.getAllRegions();
+    final String expectedJsonContent = jsonMapper.writeValueAsString(allRegions);
+    response.andExpect(status().isOk())
+      .andExpect(content().json(expectedJsonContent));
+  }
+}
