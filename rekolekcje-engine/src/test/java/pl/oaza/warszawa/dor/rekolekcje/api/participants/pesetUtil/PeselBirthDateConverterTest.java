445b8e363cfddb8c35e73ac152a270158a02df53
KTryCode <32237739+KTryCode@users.noreply.github.com>
2018-06-02 10:15:44 +0200

    Makes pesel <-> birthData test parametrized

--- rekolekcje-engine/src/test/java/pl/oaza/warszawa/dor/rekolekcje/api/participants/pesetUtil/PeselBirthDateConverterTest.java
+++ rekolekcje-engine/src/test/java/pl/oaza/warszawa/dor/rekolekcje/api/participants/pesetUtil/PeselBirthDateConverterTest.java
@@ -1,43 +1,44 @@
 package pl.oaza.warszawa.dor.rekolekcje.api.participants.pesetUtil;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@RunWith(Parameterized.class)
 public class PeselBirthDateConverterTest {
 
-  private final String pesel = "90010103999";
-  private final String correspondingBirthDate = "01.01.1990";
+  @Parameter(value = 1)
+  public String pesel;
 
-  private final String[] peselArray = {
-      "93103002331",
-      "00212011230",
-      "85022831221",
-      "05311033544",
-      "99090912324"
-  };
+  @Parameter(value = 2)
+  public String correspondingBirthDate;
 
-  private final String[] correspondingBirthDateArray = {
-      "30.10.1993",
-      "20.01.2000",
-      "28.02.1985",
-      "10.11.2005",
-      "09.09.1999"
-  };
+  @Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+        {"93103002331", "30.10.1993"},
+        {"00212011230", "20.01.2000"},
+        {"85022831221", "28.02.1985"},
+        {"05311033544", "10.11.2005"},
+        {"99090912324", "09.09.1999"}
+    });
+  }
+
 
   @Test
-  public void shouldConversionMethodReturnString() {
+  public void shouldConversionMethodReturnNotNull() {
     assertThat(PeselBirthDateConverter.convertPeselToBirthDate(pesel)).isNotEmpty();
   }
 
   @Test
   public void shouldConvertPeselToBirthDate() {
-    assertThat(
-        Arrays.stream(peselArray)
-            .map(PeselBirthDateConverter::convertPeselToBirthDate).toArray())
-        .isEqualTo(correspondingBirthDateArray);
+    assertThat(PeselBirthDateConverter.convertPeselToBirthDate(pesel)).isEqualTo(correspondingBirthDate);
   }
 }
