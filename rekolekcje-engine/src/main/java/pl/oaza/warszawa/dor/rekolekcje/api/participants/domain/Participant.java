67c93b6d9971fdde1a9d9172d78a8ccaba930182
KTryCode <32237739+KTryCode@users.noreply.github.com>
2018-06-03 11:59:34 +0200

    Refractors code to meet Karol's compliance

--- rekolekcje-engine/src/main/java/pl/oaza/warszawa/dor/rekolekcje/api/participants/domain/Participant.java
+++ rekolekcje-engine/src/main/java/pl/oaza/warszawa/dor/rekolekcje/api/participants/domain/Participant.java
@@ -4,7 +4,11 @@ import lombok.*;
 import pl.oaza.warszawa.dor.rekolekcje.api.participants.dto.*;
 import pl.oaza.warszawa.dor.rekolekcje.api.participants.value.*;
 
-import javax.persistence.*;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.*;
 
 import static pl.oaza.warszawa.dor.rekolekcje.api.participants.domain.DateConverter.*;
