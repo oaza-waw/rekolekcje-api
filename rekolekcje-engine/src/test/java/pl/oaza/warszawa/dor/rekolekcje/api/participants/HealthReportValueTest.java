f62608bf24d3a030f6436df18746261f07a7efc1
krystianl <krystian.leszczewski@nask.pl>
2018-06-05 10:08:45 +0200

    Fix name of test

--- rekolekcje-engine/src/test/java/pl/oaza/warszawa/dor/rekolekcje/api/participants/HealthReportValueTest.java
+++ rekolekcje-engine/src/test/java/pl/oaza/warszawa/dor/rekolekcje/api/participants/HealthReportValueTest.java
@@ -7,7 +7,7 @@ import pl.oaza.warszawa.dor.rekolekcje.api.participants.value.HealthReportValue;
 public class HealthReportValueTest {
 
   @Test
-  public void equalsContractForAddressValue() {
+  public void equalsContractForHealthReportValue() {
     EqualsVerifier.forClass(HealthReportValue.class)
         .verify();
   }
