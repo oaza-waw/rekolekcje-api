51a1e7dc8bd46c479139fabc9de43e443b7058a1
qiubix <qiubix@gmail.com>
2018-06-07 22:07:14 +0200

    Simplify ParticipantNotFoundException creation

--- rekolekcje-engine/src/main/java/pl/oaza/warszawa/dor/rekolekcje/api/participants/dto/ParticipantNotFoundException.java
+++ rekolekcje-engine/src/main/java/pl/oaza/warszawa/dor/rekolekcje/api/participants/dto/ParticipantNotFoundException.java
@@ -2,11 +2,7 @@ package pl.oaza.warszawa.dor.rekolekcje.api.participants.dto;
 
 public class ParticipantNotFoundException extends RuntimeException {
 
-  public ParticipantNotFoundException(long id) {
-    super("No participant with id " + id + " found!");
-  }
-
-  public ParticipantNotFoundException(ParticipantDTO dto) {
-    super("No participant found with this data: " + dto);
+  public ParticipantNotFoundException(String message) {
+    super(message);
   }
 }
