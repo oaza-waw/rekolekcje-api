ba1e094daeb360be813068091a133ca74f43dff8
qiubix <qiubix@gmail.com>
2018-06-21 22:11:38 +0200

    Fix Spring profiles setup
    Cleanup files with spring properties and default configuration.
    Enable configuring application locally via `local` profile.

--- rekolekcje-engine/src/verify/java/pl/oaza/warszawa/dor/rekolekcje/api/core/BaseIntegrationTest.java
+++ rekolekcje-engine/src/verify/java/pl/oaza/warszawa/dor/rekolekcje/api/core/BaseIntegrationTest.java
@@ -1,5 +1,7 @@
 package pl.oaza.warszawa.dor.rekolekcje.api.core;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.runner.RunWith;
@@ -7,37 +9,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@ActiveProfiles({"test", "test-local"})
 @AutoConfigureJsonTesters
 public abstract class BaseIntegrationTest {
 
-    @Autowired
-    protected JdbcTemplate jdbcTemplate;
+  @Autowired
+  protected JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    protected WebApplicationContext webApplicationContext;
+  @Autowired
+  protected WebApplicationContext webApplicationContext;
 
-    protected MockMvc mockMvc;
+  protected MockMvc mockMvc;
 
-    @Autowired
-    protected ObjectMapper jsonMapper;
+  @Autowired
+  protected ObjectMapper jsonMapper;
 
-    protected Database database;
+  protected Database database;
 
-    @Before
-    public void setup() throws Exception {
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup(webApplicationContext)
-                .apply(springSecurity())
-                .build();
-        database = new Database(jdbcTemplate);
-    }
+  @Before
+  public void setup() throws Exception {
+    mockMvc = MockMvcBuilders
+        .webAppContextSetup(webApplicationContext)
+        .apply(springSecurity())
+        .build();
+    database = new Database(jdbcTemplate);
+  }
 }
