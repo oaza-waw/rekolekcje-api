763294bbe79d5bd7d3d6748b9a2e6aa57047a083
KTryCode <krystian.lc@gmail.com>
2018-06-23 17:50:40 +0200

    Add ./env.sh script to run docker container with configured Postres with rekolekcjedb database

---
+++ env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $1 = "start" ]
+then
+
+echo "Building Postgres container...."
+docker run --name db-rekolekcje -p 5432:5432 -e POSTGRES_DB=rekolekcjedb -e POSTGRES_PASSWORD=postgres -d postgres
+
+elif [ $1 = "wipe" ]
+
+then
+
+echo "Stopping all running containers..."
+docker stop $(docker ps -a -q)
+
+echo "Removing all stopped containers..."
+docker rm $(docker ps -a -q)
+
+fi
