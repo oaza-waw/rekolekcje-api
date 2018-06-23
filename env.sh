02b6cc93381704baa4ea905e1327ce75f67d6e19
KTryCode <krystian.lc@gmail.com>
2018-06-23 18:06:19 +0200

    Make ./env.sh prettier

--- env.sh
+++ env.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-if [ $1 = "start" ]
-then
+if [ "$1" = "start" ] ; then
 
-echo "Building Postgres container...."
-docker run --name db-rekolekcje -p 5432:5432 -e POSTGRES_DB=rekolekcjedb -e POSTGRES_PASSWORD=postgres -d postgres
+  echo "Building Postgres container...."
+  docker run --name db-rekolekcje -p 5432:5432 -e POSTGRES_DB=rekolekcjedb -e POSTGRES_PASSWORD=postgres -d postgres
 
-elif [ $1 = "wipe" ]
+elif [ "$1" = "wipe" ] ; then
 
-then
+  echo "Stopping all running containers..."
+  docker stop $(docker ps -a -q)
 
-echo "Stopping all running containers..."
-docker stop $(docker ps -a -q)
-
-echo "Removing all stopped containers..."
-docker rm $(docker ps -a -q)
+  echo "Removing all stopped containers..."
+  docker rm $(docker ps -a -q)
 
 fi
