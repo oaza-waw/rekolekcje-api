858c3aff6e36557d26a5d6728567cf3bc58fd934
KTryCode <krystian.lc@gmail.com>
2018-06-24 20:58:46 +0200

    Make ./env.sh prettier

--- env.sh
+++ env.sh
@@ -3,7 +3,10 @@
 if [ "$1" = "start" ] ; then
 
   echo "Building Postgres container...."
-  docker run --name db-rekolekcje -p 5432:5432 -e POSTGRES_DB=rekolekcjedb -e POSTGRES_PASSWORD=postgres -d postgres
+  docker run --name db-rekolekcje -p 5430:5432 -e POSTGRES_DB=rekolekcjedb -e POSTGRES_PASSWORD=postgres -d postgres
+
+  echo "Building Postres container for integration tests..."
+  docker run --name db-rekolekcje-test -p 5433:5432 -e POSTGRES_DB=rekolekcjedb-test -e POSTGRES_PASSWORD=mysecretpassword -d postgres
 
 elif [ "$1" = "wipe" ] ; then
 
@@ -12,5 +15,9 @@ elif [ "$1" = "wipe" ] ; then
 
   echo "Removing all stopped containers..."
   docker rm $(docker ps -a -q)
+
+else
+  echo "Invalid command. Type './env.sh start' or './env.sh wipe'."
+  exit 0
 
 fi
